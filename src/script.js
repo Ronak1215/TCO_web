025ff3905f4425dc482f18d59cf50f87e714968d
Ronak1215 <139199657+Ronak1215@users.noreply.github.com>
2024-10-11 17:20:19 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,122 @@
+var titles = document.querySelectorAll('.swiper-slide');
+    var title = [];
+    titles.forEach(function(element) {
+  title.push(element.dataset.title);
+});
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop:true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 2.5,
+    // slideShadows: true,
+  },
+  autoplay:{
+    delay:3000,
+    disableOnInteraction:true,
+  }, 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + title[index] + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    800:{
+      depth:200,
+      modifier: .5,
+    }
+  },
+
+});
+
+var swiper2 = new Swiper(".multiple-slide-carousel", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".multiple-slide-carousel #slider-button-right",
+    prevEl: ".multiple-slide-carousel #slider-button-left",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+   1220: {
+       slidesPerView: 3,
+       spaceBetween: 30
+   },
+   670: {
+       slidesPerView: 2,
+       spaceBetween: 30
+   }
+ }
+});
+
+// Initialize the first carousel
+var thumbsSwiper1 = new Swiper(".carousel-1-thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper1 = new Swiper(".carousel-1", {
+  spaceBetween: 10,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+      swiper: thumbsSwiper1,
+  },
+});
+
+// Initialize the second carousel
+var thumbsSwiper2 = new Swiper(".carousel-2-thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".carousel-2", {
+  spaceBetween: 10,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+      swiper: thumbsSwiper2,
+  },
+});
+
+// const showNav = document.querySelector(".show-nav");
+// const closeNav = document.querySelector(".close-nav");
+// const nav = document.querySelector(".nav");
+
+// closeNav.addEventListener("click", () => {
+//   nav.classList.remove("right-0");
+//   nav.classList.add("right-[-50%]");
+// });
+
+// showNav.addEventListener("click", () => {
+//   nav.classList.remove("right-[-50%]");
+//   nav.classList.add("right-0");
+// });
+
+function toggleNavbar(collapseID){
+  document.getElementById(collapseID).classList.toggle("hidden");
+  document.getElementById(collapseID).classList.toggle("flex");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    fontFamily: {
+      'primary': ['Montserrat', 'sans-serif'],
+      'secondary': ['Roboto', 'sans-serif'],
+      // You can keep the default sans, serif, and mono definitions if needed
+      'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      'serif': ['ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+    },
+  },
+}
+
